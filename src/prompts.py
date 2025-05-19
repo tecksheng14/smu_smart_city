c42a566260fd51b5ac3343891e4d315bb1cbbac3
Teck Sheng Chia <Teck.Sheng.Chia@ibm.com>
2025-05-19 10:26:35 +0800

    adding answer prompt

--- src/prompts.py
+++ src/prompts.py
@@ -12,3 +12,19 @@ Query: {query}
 Answer: """
     
     return prompt
+
+
+def answer_prompt(document, query):
+    contexts = ""
+    for context in document:
+        contexts += f"{context.page_content}\n"
+    prompt = f"""
+    <|begin_of_text|><|start_header_id|>system<|end_header_id|>You are a helpful tax assistant in charge of providing tax advice to the users.
+Do not come out with any information not present in the context provided. Provide as much information as possible and do not have any answers in point form.
+
+Context: {contexts}
+Query: {query}
+<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+Answer: """
+    
+    return prompt
